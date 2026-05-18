7b791a1c83bfb40e70b4a9b9bc94cd0b3d3a20e9
tizips <tizipse@gmail.com>
2026-05-19 00:43:12 +0800

    chore: align test and type tooling

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,4 @@
-import { configUmiAlias, createConfig } from '@umijs/max/test';
+import { configUmiAlias, createConfig } from '@umijs/max/test.js';
 
 export default async () => {
   const config = await configUmiAlias({
